12123e591c6f25eb75e4a2c72043e249786847cd
章子轩 <salt080618@outlook.com>
2022-04-27 19:05:13 +0800

    Enter

---
+++ Game/JS/Activity.js
@@ -0,0 +1,16 @@
+var date = new Date().getDate();
+var month = new Date().getMonth() + 1;
+var day = month + "-" + date;
+switch (day) {
+    case "4-23":
+        alert("HelloWorld!")
+    case "6-18":
+        document.getElementById("theme").setAttribute("href","ActivityTheme/SaltFish_Brithday.css");
+        break;
+    case "4-23":
+        document.getElementById("theme").setAttribute("href","ActivityTheme/CaiXin_Brithday.css");
+        break;
+    case "9-3":
+        console.log("平淡的一天，却是 快乐jv君 的生日!");
+        break;
+}
