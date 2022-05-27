35954219830c5ce9fcda2ff4901e867ce18dbcbd
章子轩 <salt080618@outlook.com>
2022-05-27 21:40:24 +0800

    更新愚人节彩蛋

--- Game/JS/Activity.js
+++ Game/JS/Activity.js
@@ -12,10 +12,17 @@ switch (day) {
     case "7-13":
         document.getElementById("theme").setAttribute("href","ActivityTheme/CaiXin_Brithday.css");
         break;
-    case "9-3":
-        console.log("平淡的一天，却是 快乐jv君 的生日!");
-        break;
+    // case "9-3":
+    //     console.log("平淡的一天，却是 快乐jv君 的生日!");
+    //     break;
     case "5-8":
         document.querySelector("#thanks").querySelector("div").innerHTML = "<h1>“妈妈，母亲节快乐”<h1>";
         break;
+    case "4-1":
+        document.querySelector("body").style.transform = "rotateX(180deg)";
+        break;
+}
+function rotate() {
+    document.querySelector("body").style.transform = "rotateX(180deg)";
+    return "请不要倒着读".split("").reverse().join("");
 }
